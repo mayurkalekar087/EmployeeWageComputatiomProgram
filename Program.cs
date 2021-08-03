2322ca1a2dd54cb31dcfa10308d1669d1353bbfa
mayurkalekar087 <mayurkalekar087@gmail.com>
2021-08-03 10:25:04 +0530

    feat:Calculate daily wage

--- Program.cs
+++ Program.cs
@@ -4,18 +4,25 @@ namespace EmployeeWageComputationpr
 {
     class Program
     {
+   
         static void Main(string[] args)
         {
+            //Daily_Wage = Wage_Per_Hour * Full_Day_Hour
+            int Wage_Per_Hour = 20;
+            int Full_Day_Hour = 8;
+            int Daily_Wage = 0;
             Random rand = new Random();
             int EmpCheck =rand.Next(0, 2);
             if (EmpCheck == 0)
             {
                 Console.WriteLine("Employee is Present");
+                Daily_Wage = Wage_Per_Hour * Full_Day_Hour;
             }
             else
             {
                 Console.WriteLine("Employee is Absent");
             }
+            Console.WriteLine("Total daily wage earned " + Daily_Wage);
         }
     }
 }
