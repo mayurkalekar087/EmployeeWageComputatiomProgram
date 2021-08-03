ab555b0e0911f2c26e31551bf21d6a836ca048cb
mayurkalekar087 <mayurkalekar087@gmail.com>
2021-08-03 14:55:16 +0530

    feat:AddPartTimeEmpWage

--- Program.cs
+++ Program.cs
@@ -7,16 +7,23 @@ namespace EmployeeWageComputationpr
    
         static void Main(string[] args)
         {
+           
             //Daily_Wage = Wage_Per_Hour * Full_Day_Hour
             int Wage_Per_Hour = 20;
             int Full_Day_Hour = 8;
+            int Half_Day_Hour = 4;
             int Daily_Wage = 0;
             Random rand = new Random();
-            int EmpCheck =rand.Next(0, 2);
+            int EmpCheck =rand.Next(0, 3);
             if (EmpCheck == 0)
             {
-                Console.WriteLine("Employee is Present");
+                Console.WriteLine("Employee is PresentFullDay");
                 Daily_Wage = Wage_Per_Hour * Full_Day_Hour;
+            }
+            else if (EmpCheck == 1)
+            {
+                Console.WriteLine("Employee is PresentHalfDay");
+                Daily_Wage = Wage_Per_Hour * Half_Day_Hour;
             }
             else
             {
